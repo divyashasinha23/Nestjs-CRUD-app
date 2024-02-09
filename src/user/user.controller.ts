15003ccf80a8f737dd10f13497246a6e64dddb6f
Divyasha Sinha <divyashasinha23@gmail.com>
2024-02-09 11:57:18 +0530

    app-first-commit

---
+++ src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, UseGuards, Req} from '@nestjs/common';
+import { User } from '@prisma/client';
+import {Request} from 'express'
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
+
+@UseGuards(JwtGuard)
+@Controller('user')
+export class UserController {    
+    @Get('me')
+    getMe(@GetUser() user: User){
+        return user;
+    }
+}
